e06e4030ebb470d4f46110bc8281f2eadf5c527c
Neelesh Tiruviluamala <Neelesh.Tiruviluamala@gmail.com>
2017-09-08 14:22:40 -0700

    Updating the sandbox

--- Sandbox.py
+++ Sandbox.py
@@ -5,3 +5,10 @@ Created on Thu Sep  7 10:46:20 2017
 @author: Neel Tiruviluamala
 """
 
+import os
+import pandas as pd
+
+base_path = "C:/Users/Neel Tiruviluamala/Desktop/Zillow" #Set your basepath to be the Zillow folder
+data_path = base_path + os.sep + 'Data' + os.sep
+
+properties_2016 = pd.read_csv(data_path + 'properties_2016.csv')
